af3381f8fd380535470213c06cc1066c6a97441c
Matijs van Zuijlen <matijs@matijs.net>
2023-05-12 21:55:13 +0200

    WIP

--- glib2/ext/glib2/rbgobj_closure.c
+++ glib2/ext/glib2/rbgobj_closure.c
@@ -202,9 +202,7 @@ gr_closure_holder_free(void *data)
 
     GClosure *closure = (GClosure *)rclosure;
     gboolean last_reference = (closure->ref_count == 1);
-    if (closure->ref_count == 0) {
-      printf("Reference count is 0");
-    }
+    printf("Reference count is %i", closure->ref_count);
     g_closure_unref(closure);
     if (!last_reference) {
         g_closure_invalidate(closure);
